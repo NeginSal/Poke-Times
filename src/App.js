a13202dcb155b724018659b0ed664cf61188fd61
NeginSal <neginsaljooghi@gmail.com>
2022-01-04 20:50:56 +0330

    add react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const App = () => {
   return (
-    <div className="App">
-    <Navbar/>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="about" element={<About/> }></Route>
+        <Route path="contact" element={<Contact/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Navbar = () => {
     return (
         <nav className="nav-wrapper purple lighten-2">

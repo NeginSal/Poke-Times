c1c5dafca04b07da101a9b0f86298e4f73bee845
NeginSal <neginsaljooghi@gmail.com>
2022-01-05 21:06:54 +0330

    programmatic Redirects

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Contact</h4>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta consequuntur quia laborum quibusdam cum, blanditiis laudantium doloremque odio amet harum dolor nihil fugit id omnis saepe! Iure consectetur quasi commodi.</p>
-        </div>
-     );
-}
+  const navigate = useNavigate();
+
+  setTimeout(() => {
+    navigate("/about");
+  }, 2000);
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta
+        consequuntur quia laborum quibusdam cum, blanditiis laudantium
+        doloremque odio amet harum dolor nihil fugit id omnis saepe! Iure
+        consectetur quasi commodi.
+      </p>
+    </div>
+  );
+};
 
 export default Contact;

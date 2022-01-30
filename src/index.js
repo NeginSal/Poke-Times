85d8c50b9461c06afae7202b90a8859b33e517eb
NeginSal <neginsaljooghi@gmail.com>
2022-01-30 19:16:05 +0330

    add rootReducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import rootReducer from './reducers/rootReducer'
 
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+const initState ={
+    posts:[]
+}
+const rootReducer = (state=initState, action) => {
+    return state
+
+}
+export default rootReducer;

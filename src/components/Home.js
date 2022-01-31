ecb5bfb4470610baa7b0983afc2bac5506d45a9f
NeginSal <neginsaljooghi@gmail.com>
2022-01-31 10:16:46 +0330

    map state to props

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
+import { useSelector } from 'react-redux'
 import { Link } from "react-router-dom";
 import Pokeball from "../pokeball.png";
 
+
 const Home = () => {
-  const [posts, setPosts] = useState([]);
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => {
-        setPosts(data.slice(0, 10));
-      });
-  }, []);
+const posts = useSelector(state => state.posts)
+  // const [posts, setPosts] = useState([]);
+  // useEffect(() => {
+  //   fetch("https://jsonplaceholder.typicode.com/posts")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setPosts(data.slice(0, 10));
+  //     });
+  // }, []);
   return (
     <div className="container">
       <h4 className="center">Home</h4>
       {posts.map((post) => (
         <div className="post card" key={post.id}>
-        <img src={Pokeball} alt="A pokeball"/>
+          <img src={Pokeball} alt="A pokeball" />
           <div className="card-content">
             <Link to={`/posts/${post.id}`}>
               <span className="card-title red-text">{post.title}</span>{" "}
@@ -28,5 +31,10 @@ const Home = () => {
     </div>
   );
 };
+// const mapStateToProps = (state) => {
+//   return {
+//     posts: state.posts,
+//   };
+// };
 
 export default Home;
